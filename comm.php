44b7572f504d0707324f31df1a0ab30bbfc022bf
VJ-25 <54717395+VJ-25@users.noreply.github.com>
2020-07-03 17:54:49 +0530

    Add files via upload

---
+++ comm.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="exc.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+	<title>commodity exchange</title>
+</head>
+<body>
+	
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+			<a class="navbar-brand" href="#">ARSY</a>
+	  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	  			<span class="navbar-toggler-icon"></span>
+	  		</button>
+	  		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	  			<ul class="navbar-nav mr-auto">
+	  				<li class="nav-item active" style="padding: 10px;">
+	  					<a class="nav-link" href="comm.html">Home <span class="sr-only">(current)</span></a>
+	  				</li>
+	      			<li class="nav-item" style="padding: 10px;">
+	        			<a class="nav-link" href="members.html">Members</a>
+	      			</li>
+	      			<li class="nav-item dropdown" style="padding: 10px;">
+	        			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	        				Agricultural Products
+	        			</a>
+	        			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	        				<a class="dropdown-item" href="food.html">Food</a>
+	          				<a class="dropdown-item" href="fibre.html">Fibre</a>
+	          				<a class="dropdown-item" href="fuels.html">Fuels</a>
+	          				<a class="dropdown-item" href="raw.html">Raw Materials</a>
+	          	        </div>
+	          	    </li>
+	          	    <li class="nav-item dropdown pull-right" style="padding: 10px;" >
+	        			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	        				Login
+	        			</a>
+	        			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	        				<a class="dropdown-item" href="login.html">Login</a>
+	          				<a class="dropdown-item" href="members.html">Sign Up</a>
+	          	        </div>
+	          	    </li>
+	      			
+	      		</ul>
+
+	      		
+	    	</div>
+	    </nav>
+	    <h1 class="keta">Arsy is a regulated market that allowes trading of agricultural goods directly from the producer to You!  
+	    </h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-xl  col-lg-4 ">
+					<div >
+						<div id="why">WHY US?</div>
+						<ul>
+							<li>Discover the best offers and prices</li>
+		    				<li>Safeguards against the risk of future price fluctuations.</li>
+		    				<li>Deal for today,tomorrow, next month or next year!</li>
+		    				<li>Widens the scope of the market for the product across the country!</li>
+		    				<li>It prevents exploitation of farmers.</li>
+		    				<li>EASY TO USE!</li>
+		    			</ul>
+					</div>
+				</div>
+				<div class="col-xl col-lg-4"><img src="img1.png"></div>
+				<div class="col-xl col-lg-4 ">
+					<div >
+						<div id="why">HOW TO PROCEED?</div>
+							<ol >
+								<li>Fill the membership form.</li>
+		    					<li>Explore the products if you want to buy.</li>
+		    					<li>Post your products if you want to sell.</li>
+		    					<li>Click on the product interested and contact the dealer.</li>
+		    					<li>Negotiate</li>
+		    					<li>Tadaa! The deal is done!</li>
+		    				</ol>
+						</div>
+					</div>
+				</div>
+		</div>
+	</header>
+	<footer>
+		<div>ABOUT US</div>
+		<div>Vanshita Jain</div>
+		<div>Contact : 8344889200</div>
+		<div>Email id: jain@example.com</div>
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+
+</body>
+</html>
